d60fa1e647f11dedef9a58d8b8e9e5bf22520e7e
dat-kaihatsusha <dat-kaihatsusha@gmail.com>
2021-03-15 09:40:53 +0700

    like dislike comment 02

--- db/schema.rb
+++ db/schema.rb
@@ -124,5 +124,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_094850) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
